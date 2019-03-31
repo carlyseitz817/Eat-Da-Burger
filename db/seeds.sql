7fb0e6d47df55d4949182fdcbe60b2dae2184d1f
Carly Seitz <carlyseitz817@gmail.com>
2019-03-30 23:04:27 -0500

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Poblano Picasso', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sweet Chili O’Mine', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Freshman 15', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Kevin Bacon', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Eggers Can't be Choosers", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bohemian Radishy', true);
